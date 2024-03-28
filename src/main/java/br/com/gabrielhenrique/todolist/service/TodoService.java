e0d30df8e0af4ed70e3050e4aa108b1701f52c47
Gabriel <gabrielhenriquedps@gmail.com>
2024-03-28 02:39:05 -0300

    feat: add model mapper to map dto to entity

--- src/main/java/br/com/gabrielhenrique/todolist/service/TodoService.java
+++ src/main/java/br/com/gabrielhenrique/todolist/service/TodoService.java
@@ -2,10 +2,13 @@ package br.com.gabrielhenrique.todolist.service;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import br.com.gabrielhenrique.todolist.dto.TodoDTO;
 import br.com.gabrielhenrique.todolist.entity.Todo;
 import br.com.gabrielhenrique.todolist.repository.TodoRepository;
 
@@ -15,7 +18,12 @@ public class TodoService {
     @Autowired
     private TodoRepository todoRepository;
 
-    public List<Todo> create(){
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<Todo> create(@RequestBody TodoDTO todoDTO){
+        Todo todo = modelMapper.map(todoDTO, Todo.class);
+        todoRepository.save(todo);
         return list();
     }
     public List<Todo> update(){
